547d3b3f239ae058de137389ef108f811dd3ba0d
RaihanAldy <rhnaldy4@gmail.com>
2025-05-09 14:22:14 +0700

    9 Mei

---
+++ backend/bkpp_db.sql
@@ -0,0 +1,141 @@
+/*
+SQLyog Community v13.1.9 (64 bit)
+MySQL - 8.0.30 : Database - bkpp
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`bkpp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `bkpp`;
+
+/*Table structure for table `otp_codes` */
+
+DROP TABLE IF EXISTS `otp_codes`;
+
+CREATE TABLE `otp_codes` (
+  `otp_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `code` varchar(6) NOT NULL,
+  `purpose` enum('registration','reset_password') NOT NULL,
+  `expires_at` timestamp NOT NULL,
+  `is_used` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`otp_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `otp_codes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `otp_codes` */
+
+/*Table structure for table `place_images` */
+
+DROP TABLE IF EXISTS `place_images`;
+
+CREATE TABLE `place_images` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `place_id` int DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `place_id` (`place_id`),
+  CONSTRAINT `place_images_ibfk_1` FOREIGN KEY (`place_id`) REFERENCES `places` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `place_images` */
+
+insert  into `place_images`(`id`,`place_id`,`image_url`) values 
+(1,1,'/uploads/Transit2.jpg'),
+(2,1,'/uploads/Transit3.jpg'),
+(3,1,'/uploads/Transit4.jpg'),
+(4,1,'/uploads/Transit5.jpg'),
+(5,3,'/uploads/Besar2.jpg'),
+(6,3,'/uploads/Besar3.jpg'),
+(7,3,'/uploads/Besar4.jpg'),
+(8,3,'/uploads/Besar5.jpg'),
+(9,4,'/uploads/Gedung2.jpg'),
+(10,4,'/uploads/Gedung3.jpg'),
+(11,4,'/uploads/Gedung4.jpg'),
+(12,4,'/uploads/Gedung5.jpg'),
+(13,5,'/uploads/Kamar2.jpg'),
+(14,5,'/uploads/Kamar3.jpg'),
+(15,5,'/uploads/Kamar4.jpg'),
+(16,6,'/uploads/Lapangan2.jpg'),
+(17,6,'/uploads/Lapangan3.jpg'),
+(18,6,'/uploads/Lapangan4.jpg'),
+(19,6,'/uploads/Lapangan5.jpg');
+
+/*Table structure for table `places` */
+
+DROP TABLE IF EXISTS `places`;
+
+CREATE TABLE `places` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `place_name` varchar(255) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `trip_duration` varchar(100) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `description` text,
+  `capacity` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `places` */
+
+insert  into `places`(`id`,`place_name`,`image_url`,`trip_duration`,`price`,`description`,`capacity`) values 
+(1,'transit','/uploads/Transit.jpg','1 hari',500000.00,'Ruang Transit yang nyaman dengan fasilitas WIFI, AC, Meja & Sofa, \r\nSerta tempat perjamuan tamu.\r\n',NULL),
+(2,'meeting room kecil','/uploads/Meeting kecil.jpg','1 hari',1250000.00,'Meeting room kecil multifungsi dengan fasilitas WIFI, AC, Monitor, Alat Proyektor, \r\nMeja & kursi, serta perlengkapan sound system yang lengkap',50),
+(3,'meeting room besar','/uploads/Meeting besar.jpg','1 hari',2500000.00,'Meeting room besar yang nyaman dan luas dengan fasilitas WIFI, AC, Meja & kursi, \r\nProyektor, Layar Presentasi, serta sistem audio yang lengkap.',100),
+(4,'Gedung pertemuan','/uploads/Gedung.jpg','1 hari',3750000.00,'Ruang pertemuan yang nyaman dan luas dengan fasilitas WIFI, AC, Meja & kursi, Proyektok,\r\nLayar presentasi, serta sound system yang lengkap.',200),
+(5,'kamar','/uploads/Kamar.jpg','1 hari',200000.00,'Kamar nyaman dengan fasilitas WIFI, 1 unit AC (masing-masing kamar), \r\nSmart TV yang dilengkapi Youtube & Netflix, Double Bed. Lemari dan kamar Mandi, \r\nserta tersedia opsi smoking room untuk kenyamanan anda selama menginap.  ',NULL),
+(6,'lapangan','/uploads/Lapangan.jpg','1 hari',500000.00,'Lapangan yang luas dan nyaman, \r\ndilengkapi dengan fasilitas lengkap untuk berbagai kegiatan outdoor, termasuk area untuk olahraga,\r\nupacara, tempat duduk, serta akses parkir yang luas ',NULL);
+
+/*Table structure for table `sessions` */
+
+DROP TABLE IF EXISTS `sessions`;
+
+CREATE TABLE `sessions` (
+  `session_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `expires_at` timestamp NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `sessions` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `is_verified` tinyint(1) DEFAULT '0',
+  `is_admin` tinyint(1) DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `phone_number` (`phone_number`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`user_id`,`full_name`,`email`,`phone_number`,`password_hash`,`is_verified`,`is_admin`,`created_at`,`updated_at`) values 
+(3,'RAIHANALDY','rhnaldy4@gmail.com','+6289510889127','$2b$10$LT8Qsbzz7HNq.rUoJuQ2h.rXZ6qos5oH16N4OJ5NRpHhJrDnfL/1e',0,0,'2025-04-29 15:04:49','2025-05-05 10:05:45');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
